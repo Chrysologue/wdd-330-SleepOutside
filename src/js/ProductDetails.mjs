f658ecffcf7b2b6d95138361fe57a86d209ada7c
Chrysologue <ChrysoBeatz@gmail.com>
2025-05-25 23:42:53 +0300

    Dynamic Product Detail Page

---
+++ src/js/ProductDetails.mjs
@@ -0,0 +1,38 @@
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
+export class ProductDetails {
+    constructor(productId, dataSource) {
+        this.productId = productId;
+        this.dataSource = dataSource;
+        this.product = {};
+    }
+    addProductToCart(product) {
+        const cartItems = getLocalStorage("so-cart") || [];
+        cartItems.push(product);
+        setLocalStorage("so-cart", cartItems);
+    }
+    renderProductDetails() {
+        document.querySelector("main").innerHTML = productDetailsTemplate(this.product);
+
+    }
+    async init() {
+        this.product = await this.dataSource.findProductById(this.productId);
+        this.renderProductDetails();
+        document.getElementById('addToCart')
+            .addEventListener('click', () => this.addProductToCart(this.product));
+    }
+}
+function productDetailsTemplate(item) {
+    return `<section class="product-detail">
+        <h3>${item.Brand.Name}</h3>
+        <h2 class="divider">${item.NameWithoutBrand}</h2>
+        <img class="divider"
+                src="${item.Image}"
+                alt="${item.NameWithoutBrand}" />
+        <p class="product-card__price">$${item.FinalPrice}</p>
+        <p class="product__color">${item.Colors[0].ColorName}</p>
+        <p class="product__description">${item.DescriptionHtmlSimple}</p>
+        <div class="product-detail__add">
+            <button id="addToCart" data-id=${item.Id}>Add to Cart</button>
+        </div>
+        </section>`;
+}
